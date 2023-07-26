2b7cf14fcb1fbb320b316d1fad057b2998399b8e
ghouizi <56503937+ghouizi@users.noreply.github.com>
2023-07-26 23:13:19 +0100

    Update prompt.c

--- prompt.c
+++ prompt.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "prompt.h"
 
-char *read_command(void) {
-    char *command = NULL;
-    size_t bufsize = 0;
-    ssize_t characters;
-
-    characters = getline(&command, &bufsize, stdin);
-    if (characters == -1) { /* Handle "end of file" condition */
-        free(command);
-        return NULL;
-    }
-
-    /* Remove the newline character from the command string */
-    if (command[characters - 1] == '\n') {
-        command[characters - 1] = '\0';
-    }
-
-    return command;
+void print_prompt(void) {
+    char *cwd;
+    cwd = getcwd(NULL, 0);
+    printf("#cisfun$ ");
+    fflush(stdout);
+    free(cwd);
 }
-
