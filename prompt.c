b861a8dd207235da1afc3cfeafa7c97e68295dc3
ghouizi <ghouizi1999ilham@gmail.com>
2023-07-26 13:55:48 -0700

    task1

--- prompt.c
+++ prompt.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include "prompt.h"
 
-void display_prompt(void) {
-    printf("#cisfun$ ");
-}
-
 char *read_command(void) {
     char *command = NULL;
-    size_t bufsize = 0; /* getline will allocate memory */
+    size_t bufsize = 0;
     ssize_t characters;
 
     characters = getline(&command, &bufsize, stdin);
-    if (characters == -1) /* Handle "end of file" condition */
+    if (characters == -1) { /* Handle "end of file" condition */
+        free(command);
         return NULL;
+    }
+
+    /* Remove the newline character from the command string */
+    if (command[characters - 1] == '\n') {
+        command[characters - 1] = '\0';
+    }
 
     return command;
 }
