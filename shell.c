f788322e1dd40cc19f4ceb4e2325e02be86aef90
ghouizi <56503937+ghouizi@users.noreply.github.com>
2023-07-26 23:15:07 +0100

    Update shell.c

--- shell.c
+++ shell.c
@@ -14,7 +14,7 @@ int execute_command(char *command) {
         char *args[] = {command, NULL}; // Arguments array
         execve(command, args, NULL);
         perror("execve"); // This line is reached only if execve fails
-        exit(EXIT_FAILURE);
+        _exit(EXIT_FAILURE);
     } else if (pid > 0) {
         wait(&status); // Wait for the child process to finish
     } else {
@@ -24,4 +24,3 @@ int execute_command(char *command) {
 
     return status;
 }
-
