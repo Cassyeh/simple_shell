459b23b3d2ebb0178830d7e6af2a2c36662a8ceb
ghouizi <56503937+ghouizi@users.noreply.github.com>
2023-07-26 18:25:35 +0100

    Create betty.c

---
+++ betty.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the shell program
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int main(void)
+{
+    /* Your shell code goes here */
+    return (0);
+}
