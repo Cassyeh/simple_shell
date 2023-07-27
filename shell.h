cd02f18a768d218b36324d3ac92c3b0a68b70545
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-07-27 04:12:42 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -9,17 +9,8 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
-int strlength(char *string);
-int strcompare(char *str_1, char *str_2);
-void strcopy(char *new_str, char *old_str);
-int strspace(char *string);
-char **strtoken(char *line, char *delim);
-void free_multiarr(char **arr, int rows);
-void write_to_buff(char *string);
-char *_strcat(char *dest, char *src);
-char *newline_del(char *string);
-char *get_env(char *command);
-extern char **environ;
+#include <limits.h>
+#include <errno.h>
+extern char **__environ;
 
 #endif
